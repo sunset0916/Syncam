5f96e36170de1e36c467b56adbab1f1ea57f7278
sunset0916 <t09160306@yahoo.co.jp>
2021-11-29 15:47:40 +0900

    add firebase support

--- app/src/main/java/com/example/syncam/myDialogFragment.java
+++ app/src/main/java/com/example/syncam/myDialogFragment.java
@@ -13,6 +13,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class myDialogFragment extends DialogFragment {
 
     @NonNull
@@ -28,7 +31,6 @@ public class myDialogFragment extends DialogFragment {
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
                     }
                 })
                 .setView(editText)
