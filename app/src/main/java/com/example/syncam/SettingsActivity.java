0b47be945fa3864a13ff9b29cd4e01f8a93f0d9a
sunset0916 <t09160306@yahoo.co.jp>
2021-12-23 16:11:10 +0900

    Add comment

--- app/src/main/java/com/example/syncam/SettingsActivity.java
+++ app/src/main/java/com/example/syncam/SettingsActivity.java
@@ -9,20 +9,27 @@ import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    //画面遷移の状態を格納する変数
     boolean endFlag = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
+        //設定画面の準備
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.settings, new SettingsFragment())
                     .commit();
         }
+
+        //画面遷移の状態を格納する変数の初期化
         endFlag = false;
     }
 
+    //設定画面の項目の読み取り・反映
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -30,17 +37,20 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
+    //◁ボタンを押されたときの動作
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         HostActivity.flag = true;
     }
 
+    //画面が停止されたときの動作
     @Override
     protected void onStop() {
         super.onStop();
         if(!HostActivity.flag){
             if(!endFlag) {
+                //◁ボタン以外押下時とonRestartからのfinish()以外のonStop()時にFirebaseからルームを削除する動作
                 ReadWrite.ref.child(MainActivity.rn).removeValue();
                 MainActivity.rn = null;
             }
@@ -51,6 +61,7 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onRestart() {
         super.onRestart();
         ReadWrite.ref.get().addOnCompleteListener(task -> {
+            //Firebase上からルーム番号が削除されているときの動作
             if (!String.valueOf(Objects.requireNonNull(task.getResult()).getValue()).contains("roomNumber=" + MainActivity.rn)) {
                 finish();
                 MainActivity.rn = null;
