37611a0973692395be02404d607b8a551f34f24c
GitHub-hajimemashita20211125 <95004423+GitHub-hajimemashita20211125@users.noreply.github.com>
2021-12-23 16:53:54 +0900

    Added comment.

--- app/src/main/java/com/example/syncam/NTPTask.java
+++ app/src/main/java/com/example/syncam/NTPTask.java
@@ -14,26 +14,43 @@ import java.util.Date;
 public class NTPTask extends AsyncTask<Integer, Integer, String> {
     @Override
     protected String doInBackground(Integer... params) {
+        //NTPサーバ名の指定
         final String NTP_SERVER = "ntp.nict.jp";
+
+        //フォーマットの指定
         @SuppressLint("SimpleDateFormat") SimpleDateFormat formater = new SimpleDateFormat("HH:mm:ss.SSS");
+
+        // プロトコルの呼び出し
         NTPUDPClient client = new NTPUDPClient();
         String result = "";
         try {
+            //クライアントの呼び出しおよびNTPサーバへの接続
             client.open();
             InetAddress host = InetAddress.getByName(NTP_SERVER);
-            TimeInfo info = client.getTime(host);
 
+            //NTPサーバから時間の取得
+            TimeInfo info = client.getTime(host);
             info.computeDetails();
+
+            //Date型の宣言
             Date exactTime;
 
+            //処理時間の計算、正確な時間の取得
             exactTime = new Date(System.currentTimeMillis() + info.getOffset());
-            Log.d("Time", String.valueOf(exactTime));
+
+            //取得した時間を指定したフォーマットに変換
             result = formater.format(exactTime);
+
         } catch (Exception e) {
+
+            //スタックトレースの出力
             e.printStackTrace();
         } finally {
+
+            //クライアントの終了
             client.close();
         }
+        //フォーマットに変換した時間を返す
         return result;
     }
 }
