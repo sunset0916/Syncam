d80179a0517bcf4ed5468d8ddb8c9957083df552
sunset0916 <t09160306@yahoo.co.jp>
2022-01-27 16:53:44 +0900

    Add ManualActivity

--- app/src/main/java/com/example/syncam/GuestSettingsActivity.java
+++ app/src/main/java/com/example/syncam/GuestSettingsActivity.java
@@ -1,12 +1,17 @@
 package com.example.syncam;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.text.InputType;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
 public class GuestSettingsActivity extends AppCompatActivity {
 
@@ -26,6 +31,16 @@ public class GuestSettingsActivity extends AppCompatActivity {
 
     //設定画面の項目の読み取り・反映
     public static class SettingsFragment extends PreferenceFragmentCompat {
+
+        //GuestSettingActivityのインスタンス化
+        public GuestSettingsActivity activity;
+
+        @Override
+        public void onAttach(@NonNull Context activity) {
+            super.onAttach(activity);
+            this.activity = (GuestSettingsActivity) activity;
+        }
+
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.guest_preferences, rootKey);
@@ -35,6 +50,14 @@ public class GuestSettingsActivity extends AppCompatActivity {
             editTextPreference.setOnBindEditTextListener(editText -> {
                 editText.setInputType(InputType.TYPE_CLASS_NUMBER);
                 editText.setFilters(new InputFilter[] {new InputFilter.LengthFilter(3)});
+            });
+
+            //「詳細設定マニュアル」ボタンを押されたときの動作
+            Preference preference = findPreference("MANUAL");
+            preference.setOnPreferenceClickListener(preference1 -> {
+                Intent intent = new Intent(activity,ManualActivity.class);
+                activity.startActivity(intent);
+                return true;
             });
         }
     }
