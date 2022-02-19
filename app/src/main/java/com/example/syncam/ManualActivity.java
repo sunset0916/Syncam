fa03eaa3bfca138181650ac31e6b29318c8474d8
sunset0916 <t09160306@yahoo.co.jp>
2022-02-19 17:14:11 +0900

    Change ManualActivity

--- app/src/main/java/com/example/syncam/ManualActivity.java
+++ app/src/main/java/com/example/syncam/ManualActivity.java
@@ -2,7 +2,11 @@ package com.example.syncam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class ManualActivity extends AppCompatActivity {
 
@@ -10,5 +14,36 @@ public class ManualActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual);
+        ImageView iView1 = findViewById(R.id.iView1);
+        ImageView iView2 = findViewById(R.id.iView2);
+        //画像を縮小してImageViewに表示
+        iView1.setImageBitmap(decodeSampledBitmapFromResource(getResources(), R.drawable.img_manual_1, 500, 500));
+        iView2.setImageBitmap(decodeSampledBitmapFromResource(getResources(), R.drawable.img_manual_2, 500, 500));
+    }
+
+    //画像サイズの読み込み
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+        if (height > reqHeight || width > reqWidth) {
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
+    }
+
+    //縮小した画像の読み込み
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
     }
 }
